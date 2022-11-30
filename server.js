275402ce017591e3698dd47d3a6993ac2294a220
shivraj.bande <shivraj.bande@nslhub.com>
2022-11-30 10:25:04 +0530

    added params to url

--- server.js
+++ server.js
@@ -16,9 +16,35 @@ app.post("/",(req,res)=>{
   console.log("shivraj");
 })
 app.get("/img", (req, res) => {
-     var datetime = new Date();
-    //  console.log(datetime);
-    // console.log(req);
+  data = [
+    (obj1 = {
+      id: "12345678",
+      email: "test1@gmail.com",
+    }),
+    (obj2 = {
+      id: "234489",
+      email: "test2@gmail.com",
+    }),
+    (obj3 = {
+      id: "89021",
+      email: "test3@gmail.com",
+    }),
+    (obj4 = {
+      id: "89028991",
+      email: "test4@gmail.com",
+    }),
+  ];
+    
+    console.log(req.query.id);
+   var temp = req.query.id;
+  //  console.log(data.length);
+   for(var i =0 ;i < data.length;i++){
+    if(data[i].id == temp){
+      console.log(data[i].email);
+    }
+
+   }
+
   res.sendFile(__dirname+"/images/nslhub.jpg");
 });
 app.post("/isOpened",(req,res)=>{
